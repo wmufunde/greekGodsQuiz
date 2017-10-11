35bbf4057158d52db81e40c92262f430b9dc6c16
wmufunde <wmmufunde@yahoo.com>
2017-10-11 16:28:04 +0100

    appc ode and images

---
+++ first-project/App_Code/Submit_answer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Submit_answer
+/// </summary>
+public class Submit_answer
+{
+    public static int score = 0;
+    public Submit_answer()
+    {
+       
+    }
+}
